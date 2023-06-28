2c844c2167175b2d8101fab9c9380310a36fea55
Debby <debbykalu@gmail.com>
2023-06-28 20:14:31 +0100

    netzero third  commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../home.css'
 
 const Home = () => {
   return (
-    <div>
     <div className="container">
-    <h1 className="display-3">FARMERS COMMUNITY</h1>
-    <h2 className="display-3">Together we can unlock the secret to Net Zero Emission</h2>
-    <p className="lead">Share your Experience and challenges here!</p>
-    <div className='regLog'>
-      <a className="regBtn" href="/Register" role="button">Be a Member</a>
+      <h1 className="display-3">FARMERS COMMUNITY</h1>
+      <h2 className="display-3">Together we can unlock the secret to Net Zero Emission</h2>
+      <p className="lead">Share your Experience and challenges here!</p>
+      <div className='regLog'>
+        <Link className="regBtn" to="/Register">Be a Member</Link>
+      </div>
     </div>
-    </div>
-    </div>
-  )
+  );
 }
 
 export default Home
